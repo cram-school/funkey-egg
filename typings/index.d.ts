ad8d0240a52680b44da4ab4b5e1cc6c7bc0a261f
楼天楠 <tnlou@justhealth.cn>
2022-01-14 15:21:11 +0800

    style：添加注释

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,6 +1,9 @@
 import 'egg';
 import { Application } from 'egg';
-
+/**
+ * egg-mysql报错无法使用的解决方案
+ * https://blog.csdn.net/weixin_47348518/article/details/120699804
+ */
 declare module 'egg' {
     interface Application{
         mysql:any
